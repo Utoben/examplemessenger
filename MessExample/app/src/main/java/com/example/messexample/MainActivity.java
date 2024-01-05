8808f2958cf00f8c6ff08e8347bc36b7e6de96cd
Utoben <ubelousov15@gmail.com>
2024-01-05 19:16:48 +0500

    new intent

--- MessExample/app/src/main/java/com/example/messexample/MainActivity.java
+++ MessExample/app/src/main/java/com/example/messexample/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.messexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSendMessage = findViewById(R.id.sendbutton);
+        TextView input = findViewById(R.id.editTextMessage);
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = input.getText().toString();
+                launchNextScreen(message);
+            }
+        });
+    }
+    private void launchNextScreen(String message){
+        Intent intent = new Intent(this, ReciewedMessageActivity.class);
+        intent.putExtra("m", message);
+        startActivity(intent);
+
     }
 }
--- MessExample/app/src/main/java/com/example/messexample/ReciewedMessageActivity.java
+++ MessExample/app/src/main/java/com/example/messexample/ReciewedMessageActivity.java
@@ -2,7 +2,9 @@ package com.example.messexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReciewedMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ReciewedMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reciewed_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("m");
+        TextView textshow = findViewById(R.id.recievedText);
+        textshow.setText(message);
+
+
+
     }
 }
